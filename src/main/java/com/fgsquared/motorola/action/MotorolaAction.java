711ef21d5c0257cfb1f44584e1a812190a3c716c
macanhhuy <huymac@magrabbit.com>
2013-03-06 16:14:36 +0700

    Get user and group

--- src/main/java/com/fgsquared/motorola/action/MotorolaAction.java
+++ src/main/java/com/fgsquared/motorola/action/MotorolaAction.java
@@ -5,17 +5,21 @@ import java.util.Set;
 
 import org.apache.log4j.Logger;
 
-import vn.com.magrabbit.helloworld.action.HelloWorldAction;
-
 import com.jivesoftware.base.Group;
+import com.jivesoftware.base.GroupManager;
+import com.jivesoftware.base.User;
+import com.jivesoftware.base.UserManager;
 import com.jivesoftware.base.event.UserEvent;
 import com.jivesoftware.base.event.v2.EventListener;
-
+import com.jivesoftware.community.lifecycle.JiveApplication;
+import com.jivesoftware.community.web.soy.functions.GetJivePropertyFunction;
 public class MotorolaAction implements EventListener<UserEvent> {
+
 
 	Logger log = Logger.getLogger(MotorolaAction.class);
 	private Set<String> lstFirstCharacter;
-
+	private UserManager userManager;
+	private GroupManager groupManager;
 	@Override
 	public void handle(UserEvent userEvent) {
 
@@ -24,6 +28,22 @@ public class MotorolaAction implements EventListener<UserEvent> {
 
 		} else if (userEvent.getType().equals(UserEvent.Type.LOGGED_IN)) {
 			log.info("USER LOGGED IN!");
+			User user = userEvent.getPayload();
+			log.info("User ID:"+user.getUsername());
+			log.info("----------GROUP--------");
+			for (Group g2 : JiveApplication.getContext().getGroupManager().getUserGroups(user)) {  
+		           log.info(g2.getName());
+		        }	
+			
+		log.info("----------ALL GROUP--------");
+	     for (Group g : JiveApplication.getContext().getGroupManager().getGroups()) {  
+           log.info(g.getName());
+        }  
+		log.info("----------------------");
+//			for (Group group : getJiveContext().getGroupManager().getUserGroups(currentUser)) {
+//				log.info("Group "+ group);  
+//	        }
+			
 		}
 
 		else if (userEvent.getType().equals(UserEvent.Type.MODIFIED)) {
@@ -33,6 +53,7 @@ public class MotorolaAction implements EventListener<UserEvent> {
 
 		else if (userEvent.getType().equals(UserEvent.Type.LOGGED_OUT)) {
 			log.info("USER LOGGED_OUT!");
+			
 
 		}
 
